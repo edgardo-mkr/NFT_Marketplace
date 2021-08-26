fcc2bae74fcd8e2aa4464f692bdc0885e32e08d5
edgardo-mkr <gonzaedgardo20@gmail.com>
2021-08-26 18:31:44 -0400

    buy with dai & link
    functions:
    -buywitheth
    -buywithdai
    -buywithlink
    -adjustment on state variables for storing the offers

--- contracts/MarketplaceV1.sol
+++ contracts/MarketplaceV1.sol
@@ -1,15 +1,19 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 
 
-contract MarketplaceV1 is Initializable, Ownable{
+contract MarketplaceV1 is Initializable, OwnableUpgradeable, ReentrancyGuardUpgradeable{
     address public recipient;
     uint public fee;
     struct Offer {
+        address owner;
         address tokenAdress;
         uint tokenId;
         uint amount;
@@ -18,9 +22,11 @@ contract MarketplaceV1 is Initializable, Ownable{
         bool onSale;
     }
 
-    Offer[] public offers;
+    mapping(uint => Offer) public offers;
+    uint public offerCount;
 
-    mapping (uint => address) public offerToOwner;
+    IERC20Upgradeable daiToken;
+    IERC20Upgradeable linkToken;
 
     AggregatorV3Interface internal ethPriceFeed;
     AggregatorV3Interface internal daiPriceFeed;
@@ -29,6 +35,9 @@ contract MarketplaceV1 is Initializable, Ownable{
     function initialize(address _recipient) public initializer {
         recipient = _recipient;
         fee = 1;
+        daiToken = IERC20Upgradeable(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+        linkToken = IERC20Upgradeable(0x514910771AF9Ca656af840dff83E8264EcF986CA);
+
         ethPriceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
         daiPriceFeed = AggregatorV3Interface(0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9);
         linkPriceFeed = AggregatorV3Interface(0x2c1d072e956AFFC0D435Cb7AC38EF18d24d9127c);
@@ -45,42 +54,84 @@ contract MarketplaceV1 is Initializable, Ownable{
     function placeOffer(address _tokenAdress, uint _tokenId, uint _amount, uint32 _deadline, uint32 _usdPrice) public payable{
         IERC1155 tokenContract = IERC1155(_tokenAdress);
         require(tokenContract.isApprovedForAll(msg.sender, address(this)), "Approval is required to spend the tokens to be offered");
-        uint id = offers.push(Offer(_tokenAdress, _tokenId, _amount, block.timestamp + _deadline, _usdPrice, true)) - 1;
-        offerToOwner[id] = msg.sender;
+        offerCount++;
+        offers[offerCount] = Offer(msg.sender,_tokenAdress, _tokenId, _amount, uint32(block.timestamp + _deadline), _usdPrice, true);
     }
 
     function cancellOffer(uint _id) public {
-        require(_id < offers.length, "Offer id does not exist");
-        require(offerToOwner[_id] == msg.sender, "You are not the creator of this offer");
+        require(_id <= offerCount, "Offer id does not exist");
+        require(offers[_id].owner == msg.sender, "You are not the creator of this offer");
         offers[_id].onSale = false;
     }
 
-    function buy(uint _id, string memory _payMethod) public payable{
-        require(_id < offers.length, "Offer id does not exist");
+    function buyWithEther(uint _id) external payable nonReentrant{
+        require(_id <= offerCount, "Offer id does not exist");
 
-        address seller = offerToOwner[_id];
-        Offer memory sellerOffer = offers[_id];
+        Offer storage offerInfo = offers[_id];
 
-        require(seller != msg.sender, "The seller can't be the buyer aswell");
-        require(sellerOffer.onSale, "This offer has been cancelled");
-        require(block.timestamp <= sellerOffer.deadline, "The deadline has been reached");
+        require(offerInfo.owner != msg.sender, "The seller can't be the buyer aswell");
+        require(offerInfo.onSale, "This offer has been cancelled");
+        require(block.timestamp <= offerInfo.deadline, "The deadline has been reached");
 
-        IERC1155 tokenContract = IERC1155(sellerOffer.tokenAdress);
-        require(tokenContract.isApprovedForAll(offerToOwner[_id], address(this)), "The seller has remove aproval to spend the tokens");
+        IERC1155 tokenContract = IERC1155(offerInfo.tokenAdress);
+        require(tokenContract.isApprovedForAll(offerInfo.owner, address(this)), "The seller has remove aproval to spend the tokens");
 
-        if (_payMethod == "ETH"){
-            require(msg.value > 0, "You have not sent any ehter");
-            uint price = uint(getEthPrice() / int(sellerOffer.usdPrice));
-            require(price <= msg.value, "Not enough ether sent");
-            tokenContract.safeTransferFrom(seller,msg.sender,sellerOffer.tokenId,sellerOffer.amount, "");
-            payable(seller).call{value: price - ((price * fee) / 100)}("");
-            sellerOffer.onSale = false;
-            payable(recipient).call{value: (price * fee) / 100}("");
-            
-            if(msg.value > price){
-                payable(msg.sender).call{value: msg.value - price}("");
-            }
+        require(msg.value > 0, "You have not sent any ether");
+        uint price = uint(int(offerInfo.usdPrice) / getEthPrice());
+        require(price <= msg.value, "Not enough ether sent");
+        tokenContract.safeTransferFrom(offerInfo.owner,msg.sender,offerInfo.tokenId,offerInfo.amount, "");
+        payable(offerInfo.owner).call{value: price - (price * fee / 100)}("");
+        offerInfo.onSale = false;
+        payable(recipient).call{value: (price * fee) / 100}("");
+        
+        if(msg.value > price){
+            payable(msg.sender).call{value: msg.value - price}("");
         }
+        
+    }
+
+    function buyWithDai(uint _id, uint _amount) external payable nonReentrant{
+        require(_id <= offerCount, "Offer id does not exist");
+
+        Offer storage offerInfo = offers[_id];
+
+        require(offerInfo.owner != msg.sender, "The seller can't be the buyer aswell");
+        require(offerInfo.onSale, "This offer has been cancelled");
+        require(block.timestamp <= offerInfo.deadline, "The deadline has been reached");
+
+        IERC1155 tokenContract = IERC1155(offerInfo.tokenAdress);
+        require(tokenContract.isApprovedForAll(offerInfo.owner, address(this)), "The seller has remove aproval to spend the tokens");
+
+        require(daiToken.allowance(msg.sender, address(this)) >= _amount, "Allowance is needed to spend the dai token");
+        uint price = uint(int(offerInfo.usdPrice) / getDaiPrice());
+        require(price <= _amount, "Not enough Dai to buy the token");
+
+        tokenContract.safeTransferFrom(offerInfo.owner, msg.sender, offerInfo.tokenId, offerInfo.amount, "");
+        daiToken.transferFrom(msg.sender, offerInfo.owner, price - (price * fee / 100));
+        offerInfo.onSale = false;
+        daiToken.transferFrom(msg.sender, recipient, price * fee / 100);
+    }
+    
+    function buyWithLink(uint _id, uint _amount) external payable nonReentrant{
+        require(_id <= offerCount, "Offer id does not exist");
+
+        Offer storage offerInfo = offers[_id];
+
+        require(offerInfo.owner != msg.sender, "The seller can't be the buyer aswell");
+        require(offerInfo.onSale, "This offer has been cancelled");
+        require(block.timestamp <= offerInfo.deadline, "The deadline has been reached");
+
+        IERC1155 tokenContract = IERC1155(offerInfo.tokenAdress);
+        require(tokenContract.isApprovedForAll(offerInfo.owner, address(this)), "The seller has remove aproval to spend the tokens");
+
+        require(linkToken.allowance(msg.sender, address(this)) >= _amount, "Allowance is needed to spend the link token");
+        uint price = uint(int(offerInfo.usdPrice) / getLinkPrice());
+        require(price <= _amount, "Not enough Link to buy the token");
+
+        tokenContract.safeTransferFrom(offerInfo.owner, msg.sender, offerInfo.tokenId, offerInfo.amount, "");
+        linkToken.transferFrom(msg.sender, offerInfo.owner, price - (price * fee / 100));
+        offerInfo.onSale = false;
+        linkToken.transferFrom(msg.sender, recipient, price * fee / 100);
     }
 
 
