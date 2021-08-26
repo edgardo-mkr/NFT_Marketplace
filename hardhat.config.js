65314bff46b918d7cf38c7c32cf675d12559c7e5
edgardo-mkr <gonzaedgardo20@gmail.com>
2021-08-26 18:52:24 -0400

    env variables

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,15 @@ require('@openzeppelin/hardhat-upgrades');
  */
 
 module.exports = {
-  
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+        blockNumber: 13025254
+      }
+    }
+  },
   solidity: {
     version: "0.8.0",
     settings: {
